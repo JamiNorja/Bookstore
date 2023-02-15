c0148393b925e5d611fc95756b52531f3510c462
JamiNorja <jami.norja@outlook.com>
2023-02-15 19:23:16 +0200

    Rest commit

--- src/main/java/Bookstore/Harjoitus/web/BookController.java
+++ src/main/java/Bookstore/Harjoitus/web/BookController.java
@@ -1,5 +1,8 @@
 package Bookstore.Harjoitus.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import Bookstore.Harjoitus.domain.Book;
 import Bookstore.Harjoitus.domain.BookRepository;
@@ -29,6 +33,7 @@ private static final Logger log = LoggerFactory.getLogger(BookController.class);
 		return "index";
 	}
 	
+	//Listaa kirjat
 	@RequestMapping(value = { "/booklist", "/" })
 	public String showBooklist(Model model) {
 		log.info("get books from db");
@@ -36,6 +41,7 @@ private static final Logger log = LoggerFactory.getLogger(BookController.class);
 		return "booklist";
 	}
 	
+	//Lisää kirjan
 	@GetMapping("/addBook")
 	public String newBook(Model model) {
 		model.addAttribute("book", new Book());
@@ -43,6 +49,7 @@ private static final Logger log = LoggerFactory.getLogger(BookController.class);
 		return "newBook";
 	}
 	
+	//Tallentaa kirjan
 	@PostMapping("saveBook")
 	public String saveBook(Book book) {
 		
@@ -50,17 +57,32 @@ private static final Logger log = LoggerFactory.getLogger(BookController.class);
 		return "redirect:/booklist";
 	}
 	
+	//Poistaa kirjan
 	@GetMapping("delete/{id}")
 	public String deleteBook(@PathVariable("id") Long id) {
 		bookRepository.deleteById(id);
 		return "redirect:/booklist";
 	}
 	
+	//Muokkaa kirjan
 	@GetMapping("edit/{id}")
 	public String editBook(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("book", bookRepository.findById(id));
 		model.addAttribute("categorys", categoryRepository.findAll());
 		return "editBook";
+	}
+	
+	//Rest kaikki kirjat
+	@GetMapping("/books")
+	public @ResponseBody List<Book> showRestBooks() {
+		log.info("showRestBooks");
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	//Rest kirja id:n mukaan
+	@GetMapping("/book/{id}")
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {
+		return bookRepository.findById(id);
 	}
 	
 }
